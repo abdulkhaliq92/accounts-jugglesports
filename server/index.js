8ef2205cf214aa6a66938c6ec5409f73ab5880d8
abdulkhaliq92 <abdul84415@gmail.com>
2024-01-11 04:26:15 +0500

    added cors

--- server/index.js
+++ server/index.js
@@ -27,7 +27,11 @@ dotenv.config()
 
 app.use((express.json({ limit: "30mb", extended: true})))
 app.use((express.urlencoded({ limit: "30mb", extended: true})))
-app.use((cors()))
+app.use((cors({
+    origin: [''],
+    methods: ['POST', 'GET'],
+    credentials: true
+})))
 
 app.use('/invoices', invoiceRoutes)
 app.use('/clients', clientRoutes)
